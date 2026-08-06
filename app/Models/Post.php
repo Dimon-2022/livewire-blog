6ebae790f0e4adac712d6747ffa6e6a5ebdc7918
dima.moisieienko <dimonmail09@gmail.com>
2026-08-07 00:01:11 +0300

    initial commit + setup database, implemented roles and permissions

---
+++ app/Models/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'featured_image',
+        'status',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    //create slug
+    protected static function boot(){
+        parent::boot();
+        //model event
+        static::creating(function ($post) {
+            if(empty($post->slug)){
+                $post->slug = Str::slug($post->title);
+            }
+        });
+    }
+}
