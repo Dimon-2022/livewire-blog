4612e0288e2e1647135b63ef26ab54bfe2621cc2
dima.moisieienko <dimonmail09@gmail.com>
2026-08-07 17:25:34 +0300

    implemented index, edit, create pages for Posts including validation, conditional rendering

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::livewire('/posts', 'pages::posts.index')->name('posts.index')->middleware('can:create posts');
+
+    Route::livewire('/posts/create', 'pages::posts.create')->middleware('can:create posts')->name('posts.create');
+
+    Route::livewire('/posts/{post}/edit', 'pages::posts.edit')->name('posts.edit');
+
 require __DIR__.'/settings.php';
+});
